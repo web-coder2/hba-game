3fff5c64bbc5024013d2f22551aafecb70df03c9
web-coder <yasher18@bk.ru>
2025-10-10 16:09:31 +0300

    убрал коменты и лишний код постарасюб использовать масивы вместо mysql

--- modules/php/Game.php
+++ modules/php/Game.php
@@ -1,19 +1,6 @@
 <?php
-/**
- *------
- * BGA framework: Gregory Isabelli & Emmanuel Colin & BoardGameArena
- * hbaGame implementation : © <Your name here> <Your email address here>
- *
- * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
- * See http://en.boardgamearena.com/#!doc/Studio for more information.
- * -----
- *
- * Game.php
- *
- * This is the main file for your game logic.
- *
- * In this PHP file, you are going to defines the rules of the game.
- */
+
+
 declare(strict_types=1);
 
 namespace Bga\Games\hbaGame;
@@ -22,15 +9,6 @@ class Game extends \Bga\GameFramework\Table
 {
     public static array $CARD_TYPES;
 
-    /**
-     * Your global variables labels:
-     *
-     * Here, you can assign labels to global variables you are using for this game. You can use any number of global
-     * variables with IDs between 10 and 99. If you want to store any type instead of int, use $this->globals instead.
-     *
-     * NOTE: afterward, you can get/set the global variables with `getGameStateValue`, `setGameStateInitialValue` or
-     * `setGameStateValue` functions.
-     */
     public function __construct()
     {
 
@@ -44,158 +22,47 @@ class Game extends \Bga\GameFramework\Table
             ]
         );;
 
-        $this->cards = $this->deckFactory->createDeck('card');
-
         $this->initGameStateLabels([
             "my_first_global_variable" => 10,
             "my_second_global_variable" => 11,
         ]);        
 
-        self::$CARD_TYPES = [
-            1 => [
-                "card_name" => clienttranslate('Troll'), // ...
-            ],
-            2 => [
-                "card_name" => clienttranslate('Goblin'), // ...
-            ],
-            // ...
-        ];
-
-        /* example of notification decorator.
-        // automatically complete notification args when needed
-        $this->notify->addDecorator(function(string $message, array $args) {
-            if (isset($args['player_id']) && !isset($args['player_name']) && str_contains($message, '${player_name}')) {
-                $args['player_name'] = $this->getPlayerNameById($args['player_id']);
-            }
-        
-            if (isset($args['card_id']) && !isset($args['card_name']) && str_contains($message, '${card_name}')) {
-                $args['card_name'] = self::$CARD_TYPES[$args['card_id']]['card_name'];
-                $args['i18n'][] = ['card_name'];
-            }
-            
-            return $args;
-        });*/
     }
-
-    /**
-     * Compute and return the current game progression.
-     *
-     * The number returned must be an integer between 0 and 100.
-     *
-     * This method is called each time we are in a game state with the "updateGameProgression" property set to true.
-     *
-     * @return int
-     * @see ./states.inc.php
-     */
-
-
-     public function move_task() {
-        self::checkAction('move_task');
-    
-        $task_id = self::getArg('task_id');
-        $new_status = self::getArg('new_status');
-    
-        // Обновляем задачу
-        $sql = "UPDATE tasks SET status = '$new_status' WHERE id = $task_id";
-        self::DbQuery($sql);
-    
-        // Получаем название задачи
-        $task_title = self::getTaskTitle($task_id);
-        $player_id = self::getCurrentPlayerId();
-    
-        self::notifyAllPlayers('taskMoved', 
-            clienttranslate('${player_name} переместил задачу ${task_title}'), 
-            [
-                'player_name' => self::getPlayerName($player_id),
-                'task_title' => $task_title,
-                'task_id' => $task_id
-            ]
-        );
-    
-        self::ajaxResponse(['status' => 'ok']);
-    }
-
-
 
     public function getGameProgression()
     {
-        // TODO: compute and return the game progression
-
         return 0;
     }
 
-    /**
-     * Migrate database.
-     *
-     * You don't have to care about this until your game has been published on BGA. Once your game is on BGA, this
-     * method is called everytime the system detects a game running with your old database scheme. In this case, if you
-     * change your database scheme, you just have to apply the needed changes in order to update the game database and
-     * allow the game to continue to run with your new version.
-     *
-     * @param int $from_version
-     * @return void
-     */
     public function upgradeTableDb($from_version)
     {
-//       if ($from_version <= 1404301345)
-//       {
-//            // ! important ! Use `DBPREFIX_<table_name>` for all tables
-//
-//            $sql = "ALTER TABLE `DBPREFIX_xxxxxxx` ....";
-//            $this->applyDbUpgradeToAllDB( $sql );
-//       }
-//
-//       if ($from_version <= 1405061421)
-//       {
-//            // ! important ! Use `DBPREFIX_<table_name>` for all tables
-//
-//            $sql = "CREATE TABLE `DBPREFIX_xxxxxxx` ....";
-//            $this->applyDbUpgradeToAllDB( $sql );
-//       }
+
+
     }
 
-    /*
-     * Gather all information about current game situation (visible by the current player).
-     *
-     * The method is called each time the game interface is displayed to a player, i.e.:
-     *
-     * - when the game starts
-     * - when a player refreshes the game page (F5)
-     */
+
     protected function getAllDatas(): array
     {
         $result = [];
 
-        // WARNING: We must only return information visible by the current player.
         $current_player_id = (int) $this->getCurrentPlayerId();
 
         $result['cardsontable'] = $this->cards->getCardsInLocation('cardsontable');
 
-
-        // Get information about players.
-        // NOTE: you can retrieve some extra field you added for "player" table in `dbmodel.sql` if you need it.
         $result["players"] = $this->getCollectionFromDb(
             "SELECT `player_id` `id`, `player_score` `score` FROM `player`"
         );
 
-        // TODO: Gather all information about current game situation (visible by player $current_player_id).
-
         return $result;
     }
 
-    /**
-     * This method is called only once, when a new game is launched. In this method, you must setup the game
-     *  according to the game rules, so that the game is ready to be played.
-     */
     protected function setupNewGame($players, $options = [])
     {
-        // Set the colors of the players with HTML color code. The default below is red/green/blue/orange/brown. The
-        // number of colors defined here must correspond to the maximum number of players allowed for the gams.
+
         $gameinfos = $this->getGameinfos();
         $default_colors = $gameinfos['player_colors'];
 
         foreach ($players as $player_id => $player) {
-            // Now you can access both $player_id and $player array
             $query_values[] = vsprintf("('%s', '%s', '%s', '%s', '%s')", [
                 $player_id,
                 array_shift($default_colors),
@@ -207,58 +74,10 @@ class Game extends \Bga\GameFramework\Table
 
         $this->setGameStateInitialValue('currentHandType', 0);
 
-        // Set current trick color to zero (= no trick color)
         $this->setGameStateInitialValue('trickColor', 0);
 
-        // Mark if we already played hearts during this hand
         $this->setGameStateInitialValue('alreadyPlayedHearts', 0);
 
-        // Определение типов карт
-        $this->CARD_TYPES = [
-            2 => ['name' => '2'],
-            3 => ['name' => '3'],
-            4 => ['name' => '4'],
-            5 => ['name' => '5'],
-            6 => ['name' => '6'],
-            7 => ['name' => '7'],
-            8 => ['name' => '8'],
-            9 => ['name' => '9'],
-            10 => ['name' => '10'],
-            11 => ['name' => clienttranslate('J')],
-            12 => ['name' => clienttranslate('Q')],
-            13 => ['name' => clienttranslate('K')],
-            14 => ['name' => clienttranslate('A')]
-        ];
-
-        // Определение типов карт (суккитов)
-        $this->CARD_SUITS = [
-            1 => ['name' => clienttranslate('tech')],
-            2 => ['name' => clienttranslate('manager')],
-            3 => ['name' => clienttranslate('sale')]
-        ];
-
-        // Создаем карты
-        $cards = [];
-        foreach ($this->CARD_SUITS as $suit => $suit_info) {
-            foreach ($this->CARD_TYPES as $value => $info_value) {
-                $cards[] = [
-                    'card_type' => $suit_info['name'], // 'tech', 'manager', 'sale'
-                    'card_type_arg' => (string)$value, // 2-14
-                    'card_location' => 'deck', // или другая локация по вашему сценарию
-                    'card_location_arg' => 1, // например, номер колоды или позиция
-                    'event_type' => null, // добавьте, если потребуется
-                    'event_value' => null // добавьте, если потребуется
-                ];
-            }
-        }
-
-        // Создаем карты в базе данных
-        $this->cards->createCards($cards, 'deck');
-
-        // Create players based on generic information.
-        //
-        // NOTE: You can add extra field on player table in the database (see dbmodel.sql) and initialize
-        // additional fields directly here.
         static::DbQuery(
             sprintf(
                 "INSERT INTO player (player_id, player_color, player_canal, player_name, player_avatar) VALUES %s",
@@ -269,37 +88,15 @@ class Game extends \Bga\GameFramework\Table
         $this->reattributeColorsBasedOnPreferences($players, $gameinfos["player_colors"]);
         $this->reloadPlayersBasicInfos();
 
-        // Init global values with their initial values.
-
-        // Dummy content.
         $this->setGameStateInitialValue("my_first_global_variable", 0);
 
-        // Init game statistics.
-        //
-        // NOTE: statistics used in this file must be defined in your `stats.inc.php` file.
-
-        // Dummy content.
-        // $this->initStat("table", "table_teststat1", 0);
-        // $this->initStat("player", "player_teststat1", 0);
-
-        // TODO: Setup the initial game situation here.
-
-        // Activate first player once everything has been initialized and ready.
         $this->activeNextPlayer();
     }
 
-    /**
-     * Example of debug function.
-     * Here, jump to a state you want to test (by default, jump to next player state)
-     * You can trigger it on Studio using the Debug button on the right of the top bar.
-     */
     public function debug_goToState(int $state = 3) {
         $this->gamestate->jumpToState($state);
     }
 
-    /**
-     * Another example of debug function, to easily test the zombie code.
-     */
     public function debug_playAutomatically(int $moves = 50) {
         $count = 0;
         while (intval($this->gamestate->getCurrentMainStateId()) < 99 && $count < $moves) {
@@ -310,14 +107,4 @@ class Game extends \Bga\GameFramework\Table
             }
         }
     }
-
-    /*
-    Another example of debug function, to easily create situations you want to test.
-    Here, put a card you want to test in your hand (assuming you use the Deck component).
-
-    public function debug_setCardInHand(int $cardType, int $playerId) {
-        $card = array_values($this->cards->getCardsOfType($cardType))[0];
-        $this->cards->moveCard($card['id'], 'hand', $playerId);
-    }
-    */
 }
